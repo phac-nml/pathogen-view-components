3ca411babaa865da5a462bf90951bc33c398ce7f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-04-02 09:43:12 -0500

    🥅 fix: add demo icon fallback helper

--- demo/app/helpers/application_helper.rb
+++ demo/app/helpers/application_helper.rb
@@ -2,4 +2,14 @@
 
 # Helper methods for the demo application.
 module ApplicationHelper
+  def icon(name, library: RailsIcons.configuration.default_library, from: library, variant: nil, **arguments)
+    super
+  rescue Icons::IconNotFound
+    # Keep previews usable when an icon is referenced before that SVG is synced locally.
+    begin
+      super('arrow-right', library: library, from: from, variant: variant, **arguments)
+    rescue Icons::IconNotFound
+      tag.span('', class: arguments[:class], title: "Missing icon: #{name}", aria: { hidden: true })
+    end
+  end
 end
