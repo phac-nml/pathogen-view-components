83b2a106609530c6223fdda172b11cb49ef5ec8d
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-05-29 07:28:01 -0500

    feat(toolbar): add MenuTrigger and data merge
    Introduce Toolbar::MenuTrigger for menu-button items and a shared
    StimulusDataMerge helper. Rename button tag kwarg to button_tag and
    wire localized missing-items messaging on the toolbar root.

--- app/components/pathogen/toolbar.rb
+++ app/components/pathogen/toolbar.rb
@@ -3,16 +3,20 @@
 module Pathogen
   # Accessible toolbar primitive following the horizontal APG toolbar pattern.
   class Toolbar < Pathogen::Component
+    include Pathogen::StimulusDataMerge
+
     TOOLBAR_CLASSES = %w[
       inline-flex items-center gap-1 rounded-md border border-neutral-200 bg-neutral-50 px-1 py-1
       dark:border-neutral-700 dark:bg-neutral-900
-    ].join(' ').freeze
+    ].freeze
 
     TOOLBAR_ACTIONS = %w[
       keydown->pathogen--toolbar#handleKeyDown
       focusin->pathogen--toolbar#handleFocusIn
       click->pathogen--toolbar#handleClick:capture
     ].freeze
+
+    MISSING_ITEMS_MESSAGE_KEY = 'pathogen--toolbar-missing-items-message-value'
 
     def initialize(label: nil, labelled_by: nil, controls: nil, **system_arguments)
       @label = label
@@ -34,26 +38,24 @@ module Pathogen
     end
 
     def build_toolbar_arguments!
+      apply_toolbar_aria!
+      apply_toolbar_stimulus!
+      @system_arguments[:class] = class_names(TOOLBAR_CLASSES, @system_arguments[:class])
+    end
+
+    def apply_toolbar_aria!
       @system_arguments[:role] = 'toolbar'
       @system_arguments[:aria] ||= {}
       @system_arguments[:aria][:label] = @label if @label.present?
       @system_arguments[:aria][:labelledby] = @labelled_by if @labelled_by.present?
       @system_arguments[:aria][:controls] = @controls if @controls.present?
-
-      @system_arguments[:data] ||= {}
-      merge_data_value!(:controller, 'pathogen--toolbar')
-      merge_data_value!(:action, *TOOLBAR_ACTIONS)
-
-      @system_arguments[:class] = class_names(TOOLBAR_CLASSES, @system_arguments[:class])
     end
 
-    def merge_data_value!(key, *values)
-      existing_values = [
-        @system_arguments[:data].delete(key),
-        @system_arguments[:data].delete(key.to_s)
-      ]
-      merged = [*existing_values, *values].compact.join(' ').split.uniq.join(' ')
-      @system_arguments[:data][key] = merged
+    def apply_toolbar_stimulus!
+      @system_arguments[:data] ||= {}
+      merge_stimulus_data!(@system_arguments[:data], :controller, 'pathogen--toolbar')
+      merge_stimulus_data!(@system_arguments[:data], :action, *TOOLBAR_ACTIONS)
+      @system_arguments[:data][MISSING_ITEMS_MESSAGE_KEY] = I18n.t('pathogen.toolbar.errors.missing_items')
     end
   end
 end
