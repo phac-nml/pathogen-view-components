0f145e242be91e8a1379382b9b0a70c5ea42d222
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-05-29 09:30:24 -0500

    ♻️ refactor(toolbar): reuse toolbar button in menu trigger

--- app/components/pathogen/toolbar/menu_trigger.rb
+++ app/components/pathogen/toolbar/menu_trigger.rb
@@ -3,53 +3,41 @@
 module Pathogen
   # Menu-button trigger for toolbars. Popup content stays host-managed in v1.
   class Toolbar::MenuTrigger < Pathogen::Component
-    include Pathogen::StimulusDataMerge
-
-    TARGET_DATA_KEY = 'pathogen--toolbar-target'
-
-    TRIGGER_CLASSES = %w[
-      inline-flex items-center rounded-md border border-neutral-200 px-3 py-2 text-sm font-medium
-      text-neutral-900 dark:border-neutral-700 dark:text-neutral-100
-    ].join(' ').freeze
-
     # rubocop:disable Metrics/ParameterLists
-    def initialize(label: nil, controls: nil, expanded: false, haspopup: 'menu', id: nil, **system_arguments)
+    def initialize(
+      label: nil, controls: nil, expanded: false, haspopup: 'menu', id: nil, scheme: :default, size: :small,
+      **system_arguments
+    )
       @label = label
       @controls = controls
       @expanded = expanded
       @haspopup = haspopup
-      @system_arguments = system_arguments
-      @system_arguments[:id] = id if id.present?
+      @scheme = scheme
+      @size = size
+      @button_arguments = system_arguments
+      @button_arguments[:id] = id if id.present?
 
       build_trigger_arguments!
     end
     # rubocop:enable Metrics/ParameterLists
 
     def call
-      tag.button(**@system_arguments) { content }
+      render(Pathogen::Toolbar::Button.new(label: @label, scheme: @scheme, size: @size, **@button_arguments)) do
+        content
+      end
     end
 
     private
 
     def build_trigger_arguments!
-      @system_arguments[:type] = 'button'
-      @system_arguments[:tabindex] = -1
       apply_trigger_aria!
-      apply_trigger_data!
-      @system_arguments[:class] = class_names(TRIGGER_CLASSES, @system_arguments[:class])
     end
 
     def apply_trigger_aria!
-      @system_arguments[:aria] ||= {}
-      @system_arguments[:aria][:label] = @label if @label.present?
-      @system_arguments[:aria][:haspopup] = @haspopup
-      @system_arguments[:aria][:expanded] = @expanded
-      @system_arguments[:aria][:controls] = @controls if @controls.present?
-    end
-
-    def apply_trigger_data!
-      @system_arguments[:data] ||= {}
-      merge_stimulus_data!(@system_arguments[:data], TARGET_DATA_KEY, 'item')
+      @button_arguments[:aria] ||= {}
+      @button_arguments[:aria][:haspopup] = @haspopup
+      @button_arguments[:aria][:expanded] = @expanded
+      @button_arguments[:aria][:controls] = @controls if @controls.present?
     end
   end
 end
