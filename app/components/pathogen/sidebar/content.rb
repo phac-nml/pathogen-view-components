788a1b85443d6c96727d66765cb26e730b97bd18
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-08-12 12:35:06 -0500

    fix(sidebar): preserve accessible responsive layout

--- app/components/pathogen/sidebar/content.rb
+++ app/components/pathogen/sidebar/content.rb
@@ -6,7 +6,7 @@ module Pathogen
     class Content < Region
       BASE_CLASSES = %w[
         pathogen-sidebar-content
-        flex min-h-0 flex-1 flex-col gap-1
+        flex min-h-0 flex-1 flex-col gap-1 overflow-y-auto
         p-2
       ].join(' ').freeze
     end
