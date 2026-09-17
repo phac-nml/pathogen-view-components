938ebabd9b19988fe5d5232f4b3423396ff56cde
Eric Enns <492127+ericenns@users.noreply.github.com>
2026-09-17 10:34:40 -0500

    chore: Update devenv to 2.2.2 and update .envrc and devenv.nix to more closely match irida-next config (#193)

--- devenv.nix
+++ devenv.nix
@@ -2,31 +2,40 @@
 
 lib.mkMerge [
   {
-  # https://devenv.sh/packages/
-  packages = with pkgs; [
-    pkg-config
-    libyaml
-    openssl
-    nodejs_24
-    pnpm_10
-  ];
+    # https://devenv.sh/packages/
+    packages = with pkgs; [
+      pkg-config
+      libyaml
+      openssl
+      nodejs_24
+      pnpm_10
+    ];
 
-  # https://devenv.sh/languages/
-  languages.ruby = {
-    enable = true;
-    versionFile = ./.ruby-version;
-    bundler.enable = false;
-  };
+    # https://devenv.sh/languages/
+    languages.ruby = {
+      enable = true;
+      versionFile = ./.ruby-version;
+      bundler.enable = false;
+    };
 
-  languages.javascript = {
-    enable = true;
-    package = pkgs.nodejs_24;
-    corepack.enable = true;
-  };
+    languages.javascript = {
+      enable = true;
+      package = pkgs.nodejs_24;
+      corepack.enable = true;
+    };
 
-  # Install ruby-lsp in the shell so the Ruby LSP extension works
-  enterShell = ''
-    gem install ruby-lsp
-  '';
-}
+    # Install ruby-lsp in the shell so the Ruby LSP extension works
+    enterShell = ''
+      gem install ruby-lsp
+    '';
+  }
+  (lib.mkIf pkgs.stdenv.isLinux {
+    enterShell = ''
+      # add libnss_sss.so.2 to LD_PRELOAD if it exists, to avoid issues with SSSD and NSS
+      LIBNSS_SSS_PATH=$(find /lib /usr/lib /lib64 /usr/lib64 -name "libnss_sss.so.2" 2>/dev/null | head -n 1)
+      if [ -n "$LIBNSS_SSS_PATH" ]; then
+        export LD_PRELOAD="$LIBNSS_SSS_PATH"
+      fi
+    '';
+  })
 ]
